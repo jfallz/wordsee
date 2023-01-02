79a2110819a501ac883ee0b5c9d6e395c5047e0d
James Johnson <james@localhost.localdomain>
2023-01-02 09:00:17 -0800

    modified:   data.txt 	renamed:    testing.py -> wordsee.py

--- wordsee.py
+++ wordsee.py
@@ -1,11 +1,14 @@
 import sys
 import os
+
 F_NAME = "data.txt"
+WORD_LIST = []
 
 # clear the datafile's text
 def clearData():
     with open(F_NAME, 'w'):
         pass
+    print("File has been cleared.")
 
 # parsing cli arguments
 for i in range(1, len(sys.argv)):
@@ -15,9 +18,10 @@ for i in range(1, len(sys.argv)):
         F_NAME = sys.argv[i+1]
 
 # now creating vector for word storage, and traversing file contents 
-
 try:
     with open(F_NAME, 'r') as f:
-        print("a")
+        for line in f:
+            WORD_LIST.extend(line.split())
+        print("List successfully generated.")
 except IOError:
     print("File '" + F_NAME + "' not found.")
